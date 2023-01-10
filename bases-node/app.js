ce65f8389e1b32608186c18f5b6d951a3e380eae
Adrian <adotel15@hotmail.com>
2023-01-10 18:38:26 +0100

    Added yarns

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,13 +1,34 @@
 
 const { crearTabla } = require('./helpers/multiplicar')
 
+const argv = require('yargs')
+                .option('b', 
+                {
+                    alias: 'base',
+                    type: 'number',
+                    demandOption: true,
+                    describe: 'Es la base de la tabla de multiplicar'
+                })
+                .option('l',
+                {
+                    alias: 'listar',
+                    type: 'boolean',
+                    default: false,
+                    describe: 'Muestra la tabla en consola'
+                })
+                .check( (argv, options) => {
+                    if( isNaN(argv.b) ) throw 'La base tiene que ser un número';
+                    return true;
+                })
+                .argv
+
 console.clear();
 
 // process.argv es lo que lee lo que viene del input de la consola
-const [,, arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+// const [,, arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
 
 
-crearTabla(base)
+crearTabla(argv.base, argv.listar)
     .then( result => console.log(result))
     .catch( error => console.log(error))
--- bases-node/helpers/multiplicar.js
+++ bases-node/helpers/multiplicar.js
@@ -1,18 +1,22 @@
 
 const fs = require('fs'); 
 
-const crearTabla = async (base = 5) => {
+const crearTabla = async (base = 5, listar) => {
 
     try {
 
-        console.log("==================");
-        console.log(`  Tabla del ${base}`);
-        console.log("==================");
-    
         let salida = '';
         for(let i = 1; i < 11; ++i)  salida += `${base} x ${i} = ${ base * i }\n`;
-        
-        console.log(salida)
+
+
+        if(listar) {
+            console.log("==================");
+            console.log(`  Tabla del ${base}`);
+            console.log("==================");
+            
+            console.log(salida)
+        }
+
         fs.writeFileSync(`tabla-${base}.txt`, salida);
         return `Tabla ${base} creada`;
 
