596a039728293df8ec18ba17b7483d75b1ad10cc
Adrian <adotel15@hotmail.com>
2023-01-10 18:41:20 +0100

    Exported argv, for module use

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,26 +1,7 @@
 
-const { crearTabla } = require('./helpers/multiplicar')
 
-const argv = require('yargs')
-                .option('b', 
-                {
-                    alias: 'base',
-                    type: 'number',
-                    demandOption: true,
-                    describe: 'Es la base de la tabla de multiplicar'
-                })
-                .option('l',
-                {
-                    alias: 'listar',
-                    type: 'boolean',
-                    default: false,
-                    describe: 'Muestra la tabla en consola'
-                })
-                .check( (argv, options) => {
-                    if( isNaN(argv.b) ) throw 'La base tiene que ser un número';
-                    return true;
-                })
-                .argv
+const { crearTabla } = require('./helpers/multiplicar')
+const argv = require('./config/yargs')
 
 console.clear();
 
