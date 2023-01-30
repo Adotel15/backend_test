cce260b15c06e549d847cbfd51f6f3ce34bcc5cc
Adotel <adriandotel@gmail.com>
2023-01-30 22:06:51 +0100

    Html and CSS in the server .get

--- web_server/app.js
+++ web_server/app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// Static content
+// .use (Para ejecutar middelwares)
+// .get (para cuando se hacen peticiones al servidor en rutas concretas)
+app.use( express.static('public') );
+
 app.get('/', (req, res) => {
     res.send("Home page")
 });
@@ -12,8 +17,9 @@ app.get('/home', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.send('404 | Page not found')
-})
+    res.sendFile( __dirname + '/public/404.html')
+});
+
 app.listen(port, () => {
     console.log("Servidor levantado puerto 8080");
 });
